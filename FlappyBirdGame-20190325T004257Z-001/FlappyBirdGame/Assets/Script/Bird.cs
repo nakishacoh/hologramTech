242dd191587b3860c9756efa0dce90cc7c48561c
Chris-ric-browne <42975420+Chris-ric-browne@users.noreply.github.com>
2019-03-26 19:17:09 -0400

    commit

---
+++ FlappyBirdGame-20190325T004257Z-001/FlappyBirdGame/Assets/Script/Bird.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Bird : MonoBehaviour
+{
+    public float upForce = 200f;                   //Upward force of the "flap".
+    private bool isDead = false;            //Has the player collided with a wall?
+
+    private Rigidbody2D rb2d;               //Holds a reference to the Rigidbody2D component of the bird.
+    private Animator anim;
+    void Start()
+    {
+
+        //Get and store a reference to the Rigidbody2D attached to this GameObject.
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+       
+    }
+
+    void Update()
+    {
+        //Don't allow control if the bird has died.
+        if (isDead == false)
+        {
+            //Look for input to trigger a "flap".
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger("Flap");
+
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        rb2d.velocity = Vector2.zero;
+        isDead = true;
+        anim.SetTrigger("Die");
+        GameControl.instance.BirdDied();
+
+
+ 
+    }
+}
